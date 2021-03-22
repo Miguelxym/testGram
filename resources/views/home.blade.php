09454efbf7c558148d4679bbed37a3553503f978
Miguelxym <miguelsampaio149@gmail.com>
2021-03-22 21:12:25 +0000

    base code

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <img src="https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-19/s150x150/18160536_277188729358000_2615606521731481600_a.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&_nc_ohc=UWR8-wfG4bQAX8zv_vM&tp=1&oh=f06ba2439e04d3a987c816b08fcab552&oe=60379D61" alt="User Logo" class="rounded-circle p-5" style="height: 200px">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>insoniascarvao</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>2.200</strong> Posts</div>
+                <div class="pr-5"><strong>226k</strong> Followers</div>
+                <div class="pr-5"><strong>593</strong> Following</div>
+            </div>
+            <div class="pt-4"><strong>Insónias em Carvão</strong></div>
+            <div>Memearia do Bairro </div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4"><img class="w-100" alt="Photo by Insónias em Carvão on January 24, 2021. A imagem pode conter: uma ou mais pessoas." srcset="https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-15/e35/c0.89.1417.1417a/s150x150/141213121_453020325878157_4214804909479394056_n.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&amp;_nc_cat=109&amp;_nc_ohc=CMvfR94hceMAX8Dzv7C&amp;tp=1&amp;oh=71a4d14f3f67531f1bcb09eeeef805f3&amp;oe=60395393 150w,https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-15/e35/c0.89.1417.1417a/s240x240/141213121_453020325878157_4214804909479394056_n.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&amp;_nc_cat=109&amp;_nc_ohc=CMvfR94hceMAX8Dzv7C&amp;tp=1&amp;oh=5672aaff0d30385aecc74644dd882e1d&amp;oe=6038CC15 240w,https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-15/e35/c0.89.1417.1417a/s320x320/141213121_453020325878157_4214804909479394056_n.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&amp;_nc_cat=109&amp;_nc_ohc=CMvfR94hceMAX8Dzv7C&amp;tp=1&amp;oh=f34b62cb9337839d5aadfdd5947a7b06&amp;oe=603713EB 320w,https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-15/e35/c0.89.1417.1417a/s480x480/141213121_453020325878157_4214804909479394056_n.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&amp;_nc_cat=109&amp;_nc_ohc=CMvfR94hceMAX8Dzv7C&amp;tp=1&amp;oh=de21178d402cf59c08c15e06efe31d6e&amp;oe=6038D82E 480w,https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.89.1417.1417a/s640x640/141213121_453020325878157_4214804909479394056_n.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&amp;_nc_cat=109&amp;_nc_ohc=CMvfR94hceMAX8Dzv7C&amp;tp=1&amp;oh=7147ec3b5f59a340fe3b83093701bbad&amp;oe=6035BA86 640w" src="https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.89.1417.1417a/s640x640/141213121_453020325878157_4214804909479394056_n.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&amp;_nc_cat=109&amp;_nc_ohc=CMvfR94hceMAX8Dzv7C&amp;tp=1&amp;oh=7147ec3b5f59a340fe3b83093701bbad&amp;oe=6035BA86"></div>
+        <div class="col-4"><img class="w-100" alt="Photo by Insónias em Carvão on January 24, 2021. A imagem pode conter: uma ou mais pessoas." srcset="https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-15/e35/140061178_232934418283211_4828799062318619975_n.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&_nc_cat=111&_nc_ohc=lkH-bMWOk1gAX9uMItQ&tp=1&oh=85c75f26032cca5086ab620b54eb4d56&oe=603744A7"></div>
+        <div class="col-4"><img class="w-100" alt="Photo by Insónias em Carvão on January 24, 2021. A imagem pode conter: uma ou mais pessoas." srcset="https://instagram.fopo2-2.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c106.0.1227.1227a/s640x640/133830040_121133189853040_6294909354169050087_n.jpg?_nc_ht=instagram.fopo2-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=w0PywX_0NlQAX9fdlDb&tp=1&oh=e2b95c9ef7a7f4762ca6cfca326ffcde&oe=6039293A"></div>
+    </div>
+</div>
+@endsection

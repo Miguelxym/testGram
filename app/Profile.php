212cf4fa0e94cd89f25d56836726495e68feff61
Miguelxym <miguelsampaio149@gmail.com>
2021-03-30 01:16:02 +0100

    Vue components for follows and default image

--- app/Profile.php
+++ app/Profile.php
@@ -9,6 +9,12 @@ class Profile extends Model
     //disable mass assignment
     protected $guarded = [];
 
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/D1nEQHuuym1mbR6YsdietFhxJ4lORNPa0C9UUo4R.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,12 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3">
-            <img src="https://scontent-lis1-1.cdninstagram.com/v/t51.2885-19/s150x150/18160536_277188729358000_2615606521731481600_a.jpg?tp=1&_nc_ht=scontent-lis1-1.cdninstagram.com&_nc_ohc=2zonF7uyl-YAX-GbwkD&ccb=7-4&oh=720063dcf6e09568add957c15f1fca3c&oe=6082C2E1&_nc_sid=7bff83" alt="User Logo" class="rounded-circle p-5" style="height: 200px">
+        <div class="col-3 p-5">
+            <img src="{{ $user->profile->profileImage() }}" alt="User Logo" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user->username }}</h1>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user->username }}</div>
+
+                    <follow-button user-id="{{ $user->id }}"></follow-button>
+                </div>
+
                 @can('update', $user->profile)
                     <a href="/p/create">Add Post</a>
                 @endcan
